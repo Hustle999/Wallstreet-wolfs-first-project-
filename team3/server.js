69b017d927364977fce23ffb9781a8b76fd0c8c2
Сэнгэдорж Өлзий-Баяр <24LP7143@G401-CC0116.local>
2024-09-24 12:33:05 +0800

    next app uusgev

--- team3/server.js
+++ team3/server.js
@@ -1,10 +1,10 @@
-const http=require('http');
-const port=2222
-const server=http.createServer((request,response)=>{
-  response.statusCode=200;
-  response.setHeader("Content-type","text/plain")
-  response.end("I don't understand it")  
-})
-server.listen(port,()=>{
-    console.log("Server started running")
-})
+const http = require("http");
+const port = 2222;
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader("Content-type", "text/plain");
+  response.end("I don't understand it");
+});
+server.listen(port, () => {
+  console.log(`Server started running port:http`);
+});

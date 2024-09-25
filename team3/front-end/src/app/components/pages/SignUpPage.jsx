17a55561cd09553ea09ee77fdee120e24932341e
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2024-09-25 15:15:39 -0700

    input values are given in front-end

--- team3/front-end/src/app/components/pages/SignUpPage.jsx
+++ team3/front-end/src/app/components/pages/SignUpPage.jsx
@@ -2,6 +2,22 @@ import { GeldIcon } from "../Icons/Geld";
 import Link from "next/link";
 
 export const SignUpPage = () => {
+ const url="http://localhost:1234/sign-up"
+ const onSingUp=async (e)=>{
+const user={
+  name:e.target.name.value,
+  email:e.target.email.value,
+  password:e.target.password.value
+} 
+const options={
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json"
+  },
+  body:user
+}
+
+ }
   return (
     <div className="w-full flex items-center">
       <div className="w-[50%]  flex justify-center items-center">
@@ -14,21 +30,23 @@ export const SignUpPage = () => {
             <h1>Create Geld account</h1>
           </div>
           <h2>Sign up below to create your Wallet account</h2>
+          <form className="flex flex-col justify-center items-center gap-5" >
           <label className="w-[350px] bg-[#F3F4F6] input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Name" />
+            <input type="text" name="name" className="grow" placeholder="Name" />
           </label>
           <label className="bg-[#F3F4F6] w-[350px] input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Email" />
+            <input type="text" name="email" className="grow" placeholder="Email" />
           </label>
           <label className="bg-[#F3F4F6] w-[350px] input input-bordered flex items-center gap-2">
-            <input type="password" className="grow" placeholder="Password" />
+            <input type="password" name="password" className="grow" placeholder="Password" />
           </label>
           <label className="bg-[#F3F4F6] w-[350px] input input-bordered flex items-center gap-2">
-            <input type="password" className="grow" placeholder="Re-password" />
+            <input type="password" name="re-password" className="grow" placeholder="Re-password" />
           </label>
-          <button className="w-[350px] btn bg-[#0166FF] rounded-[20px]">
-            Log In
+          <button className="w-[350px] btn bg-[#0166FF] rounded-[20px]" >
+            Sign up
           </button>
+          </form>
           <div className="flex gap-4">
             <h1>Already have account?</h1>
             <Link className="text-blue-600" href={"/"}>

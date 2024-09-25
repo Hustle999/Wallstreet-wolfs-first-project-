ea07148fec274e209faf98860bdea841df951adf
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-09-25 22:10:56 +0800

    teamTwo backend is slightly fixed

--- team3/front-end/src/app/components/pages/LoginPage.jsx
+++ team3/front-end/src/app/components/pages/LoginPage.jsx
@@ -1,6 +1,26 @@
+"use client"
 import { GeldIcon } from "../Icons/Geld";
 import Link from "next/link";
 export const LoginPage = () => {
+  const url="http://localhost:1234/sign-in"
+  const onSubmit= async(e)=>{
+    e.preventDefault()
+const user={
+email:e.target.email.value,
+password:e.target.password.value
+}
+const options={
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json"
+  },
+  body:JSON.stringify(user)
+}
+const response=await fetch(url,options)
+const data=await response.json()
+console.log("Ima working")
+console.log(data)
+  }
   return (
     <div className="w-full flex items-center">
       
@@ -14,20 +34,21 @@ export const LoginPage = () => {
               <h1>Welcome Back</h1>
             </div>
             <h2>Welcome back, Please enter your details</h2>
+            <form action="" className="flex flex-col justify-center items-center gap-5" onSubmit={onSubmit}>
             <label className="w-[350px] bg-[#F3F4F6] input input-bordered flex items-center gap-2">
-              <input type="text" className="grow" placeholder="Email" />
+              <input type="email" name="email" className="grow" placeholder="Email" required/>
             </label>
             <label className="bg-[#F3F4F6] w-[350px] input input-bordered flex items-center gap-2">
-              <input type="password" className="grow" placeholder="password" />
+              <input type="password" name="password" className="grow" placeholder="password" required/>
             </label>
-            <button className="w-[350px] btn bg-[#0166FF] rounded-[20px]">
+            <button type="submit" className="w-[350px] btn bg-[#0166FF] rounded-[20px]" >
               Log In
             </button>
+            </form>
             <div className="flex gap-4">
               <h1>Don’t have account?</h1>
-              <Link href={`sign-up`}><a className="text-blue-600" href="">
-                Sign up
-              </a>  </Link>
+              <Link className="text-blue-600" href={`sign-up`}>
+              Sign up  </Link>
             </div>
           </div>
         </div>

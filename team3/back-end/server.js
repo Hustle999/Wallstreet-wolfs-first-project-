4b1bb5914e9e4d004e487bfc18d6ddbe94ce24d9
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2024-09-25 13:54:34 -0700

    backend editing

--- team3/back-end/server.js
+++ team3/back-end/server.js
@@ -1,10 +1,23 @@
-const http = require("http");
-const port = 2222;
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader("Content-type", "text/plain");
-  response.end("I don't understand it");
-});
-server.listen(port, () => {
-  console.log(`Server started running port:http`);
-});
+import express from "express"
+import bodyParser from "body-parser";
+import cors from 'cors'
+import fs from 'fs'
+const port =1234;
+const app=express()
+app.get("/",(request,response)=>{
+  response.json(
+    {
+      greeting:"Hello Iam get"
+    }
+  )
+})
+app.post("/",(request,response)=>{
+  response.json(
+    {
+      greeting:"Hello Iam Post"
+    }
+  )
+})
+app.listen(port,()=>{
+  console.log(`Server is started working`)
+})
